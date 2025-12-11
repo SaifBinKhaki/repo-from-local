3c19bf8e7bd5060ab56767fe3c635ca0954102fc
Saif Ullah Bin Khaki <113775405+Devigners@users.noreply.github.com>
2025-12-11 13:10:13 +0100

    Adding new files

---
+++ main2.py
@@ -0,0 +1,11 @@
+def sum():
+    total = 0
+    for i in range(1, 11):
+        total += i
+    return total
+
+
+print("The sum of numbers from 1 to 10 is:", sum())
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
---
+++ sum.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+
+def sum():
+    total = 0
+    for i in range(1, 11):
+        total += i
+    return total
+
+
+print("The sum of numbers from 1 to 10 is:", sum())
